6ab100b3e623c7726f8251f0069acbb343bf7295
PedrOliveiraM <pedropucmont@gmail.com>
2025-02-19 09:25:45 -0300

    feat: Set up PostgreSQL and Redis clients with initial migrations and environment configuration

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,12 @@
-import z from "zod"
+import z from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  POSTGRES_URL: z
+    .string()
+    .url()
+    .default('postgres://docker:docker@localhost:5432/postgres'),
+  REDIS_URL: z.string().url().default('redis://localhost:6379'),
 })
 
 export const env = envSchema.parse(process.env)
